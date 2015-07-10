eee464003803e50606dcc25636b18ddf0b427a29
Pete Lower <pete.lower@gmail.com>
2015-07-09 17:23:10 -0700

    update header arrangement, add border image for border on nav

--- wordpress/wp-content/themes/maudience-luckypawz/header.php
+++ wordpress/wp-content/themes/maudience-luckypawz/header.php
@@ -34,14 +34,14 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
+			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu  inner-wrap' ) ); ?>
+		</nav><!-- #site-navigation -->
+
 		<div id="inner-header-wrap" class="inner-header-wrap inner-wrap">
-
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
-				<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
-
 			<?php if ( get_header_image() ) : ?>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
 			<?php endif; ?>
@@ -50,9 +50,8 @@
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</hgroup>
-
-			
 		</div><!-- #inner-header-wrap -->
+
 	</header><!-- #masthead -->
 
 	<div id="main" class="wrapper">
